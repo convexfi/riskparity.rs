7006940a0d2705bbf50d4d6764c3d1db19d1f3ae
mirca <jvmirca@gmail.com>
2022-10-11 22:13:35 +0800

    implement vanilla method by Choi 2022

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,34 @@
-fn compute_riskparity_ccd() {
+pub mod vanilla {
     // implement risk parity algo by Choi 2022
+    pub fn compute_riskparity_ccd_choi(
+        cov: &ndarray::Array2<f64>,
+        budget: &ndarray::Array1<f64>,
+        maxiter: i32,
+        tol: f64,
+    ) -> ndarray::Array1<f64> {
+        let n = budget.len();
+        let volatility = cov.diag().mapv(f64::sqrt);
+        let invvol = ndarray::Array2::from_diag(&volatility.mapv(|a| 1.0 / a));
+        let corr = (invvol.dot(cov)).dot(&invvol.t());
+        let mut adj = corr.to_owned();
+        for i in 0..n {
+            adj[[i, i]] = 0.0;
+        }
+        let mut wk = ndarray::Array1::<f64>::ones(n);
+        wk = wk.mapv(|a| a / corr.sum());
+        let mut aux = ndarray::Array1::<f64>::zeros(n);
+        for _k in 0..maxiter {
+            aux = 0.5 * adj.dot(&wk);
+            wk = (aux.mapv(|a| a.powi(2)) + budget).mapv(f64::sqrt) - aux;
+            if (wk.clone() * (corr.dot(&wk.clone())) - budget)
+                .mapv(f64::abs)
+                .mean()
+                < Some(tol)
+            {
+                break;
+            }
+        }
+        let w = wk / volatility;
+        w.mapv(|a| a / w.sum())
+    }
 }
